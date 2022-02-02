1a81f0c1d23d6b6e704518d129f0071fb36d9f5e
ChiaMineJP <admin@chiamine.jp>
2022-02-03 00:57:53 +0900

    Skip installing python39 on RH like OS if it is already installed

--- install.sh
+++ install.sh
@@ -113,7 +113,9 @@ if [ "$(uname)" = "Linux" ]; then
 	elif type yum && [ -f "/etc/redhat-release" ] || [ -f "/etc/centos-release" ] || [ -f "/etc/fedora-release" ]; then
 		# CentOS or Redhat or Fedora
 		echo "Installing on CentOS/Redhat/Fedora."
-		sudo yum install -y python39
+		if ! command -v python3.9 >/dev/null 2>&1; then
+			sudo yum install -y python39
+    fi
 	fi
 elif [ "$(uname)" = "Darwin" ] && ! type brew >/dev/null 2>&1; then
 	echo "Installation currently requires brew on MacOS - https://brew.sh/"
