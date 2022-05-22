28a0e06b2a394d0e76aa35c164db9700ac59c8d6
neurosis69 <83925572+neurosis69@users.noreply.github.com>
2022-05-22 09:01:37 +0200

    new test

--- chia/full_node/full_node.py
+++ chia/full_node/full_node.py
@@ -198,7 +198,7 @@ class FullNode:
             await self.db_wrapper.add_connection(c)
 
         await (await db_connection.execute("pragma journal_mode=wal")).close()
-        db_sync = db_synchronous_on(self.config.get("db_sync", "auto"), self.db_path)
+        db_sync = "normal"
         self.log.info(f"opening blockchain DB: synchronous={db_sync}")
         await (await db_connection.execute("pragma synchronous={}".format(db_sync))).close()
 
