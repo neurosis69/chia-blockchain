da2c5cb81bdf729cce674f83447f983d6cae6cc6
neurosis69 <thomashein@gmx.at>
2022-05-27 12:00:50 +0200

    test with wal mode

--- chia/full_node/full_node.py
+++ chia/full_node/full_node.py
@@ -197,7 +197,7 @@ class FullNode:
                 await c.set_trace_callback(sql_trace_callback)
             await self.db_wrapper.add_connection(c)
 
-        await (await db_connection.execute("pragma journal_mode=off")).close()
+        await (await db_connection.execute("pragma journal_mode=wal")).close()
         db_sync = "off"
         self.log.info(f"opening blockchain DB: synchronous={db_sync}")
         await (await db_connection.execute("pragma synchronous={}".format(db_sync))).close()
