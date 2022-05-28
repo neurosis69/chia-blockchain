c825217be9ae1cf527782413c3341139bf860705
neurosis69 <83925572+neurosis69@users.noreply.github.com>
2022-05-29 00:05:09 +0200

    change chache_size

--- chia/full_node/full_node.py
+++ chia/full_node/full_node.py
@@ -197,7 +197,7 @@ class FullNode:
                 await c.set_trace_callback(sql_trace_callback)
             await self.db_wrapper.add_connection(c)
 
-        await (await db_connection.execute("pragma cache_size=-500000")).close()
+        await (await db_connection.execute("pragma cache_size=-100000")).close()
         await (await db_connection.execute("pragma journal_mode=wal")).close()
         db_sync = db_synchronous_on(self.config.get("db_sync", "auto"), self.db_path)
         self.log.info(f"opening blockchain DB: synchronous={db_sync}")
